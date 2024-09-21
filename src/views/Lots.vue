8b1fd5b6fe4a44ba01a60d2fc386b5f01a5f5455
Roma <>
2024-09-22 01:06:37 +0800

    lots list

--- src/views/Lots.vue
+++ src/views/Lots.vue
@@ -1,7 +1,65 @@
 <template>
-	
+	<div>
+		<div class="d-flex align-items-center justify-content-between mb-4 pt-3">
+			<div class="fs-5 fw-bold">
+				Lots
+			</div>
+			<a href="#" class="d-flex align-items-center fw-bold small" @click.prevent="getList()">
+				<span>Reload</span>
+			</a>
+		</div>
+
+		<div class="container">
+			<div class="row gx-3">
+				<div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-2 d-flex flex-column align-items-stretch"
+					v-for="lot in lots">
+					<div class="d-flex flex-column align-items-between h-100 border shadow-sm p-3">
+						<LotInfo :lot="lot" :key="`${lot.id}-${lot.fetchTimestamp}`" />
+						<router-link :to="`/lot/${lot.id}`" class="text-end mt-2">
+							Enter
+						</router-link>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
+import LotInfo from './lot/LotInfo.vue'
+import { onMounted, ref, inject, onUnmounted } from 'vue';
+import errorMessage from '@/helpers/errorMessage'
+import { getLot } from '@/views/lot/lot.js'
 
+const $swal = inject('$swal')
+const $web3 = inject('$web3')
+
+const lots = ref([])
+
+onMounted(async () => {
+	await getList()
+})
+
+const getList = async () => {
+	try {
+		const lotsCount = (await $web3.contract('auction').instance.lotIdx()).toString()
+		for (let id = lotsCount; id > 0; id--) {
+			const lot = await getLot(id)
+			const idx = lots.value.find(l => l.id == id)
+			if (idx) {
+				lots.value[idx] = lot
+			} else {
+				lots.value.push(lot)
+			}
+		}
+	} catch (error) {
+		console.error(error)
+		$swal.fire({
+			icon: 'error',
+			title: 'Lots fetch error',
+			text: errorMessage(error),
+			timer: 3000,
+		});
+	}
+}	
 </script>
---
+++ src/views/lot/LotInfo.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="d-flex justify-content-between">
+        <span class="me-2">Lot Id:</span>
+        {{ lot.id }}
+    </div>
+
+    <div class="d-flex justify-content-between">
+        <span class="me-2">Creator:</span>
+        <a :href="`${$web3.blockExplorer}/address/${lot.creator}`" target="_blank" rel="noopener noreferrer">
+            {{ $web3.addressShort(lot.creator) }}
+        </a>
+    </div>
+
+    <div class="d-flex justify-content-between">
+        <span class="me-2">Status:</span>
+        <template v-if="!isClosed(lot)">
+            <span v-if="isActive(lot)">Bidding</span>
+            <span v-if="isRevealing(lot)">Revealing</span>
+        </template>
+        <span v-else>Closed</span>
+    </div>
+
+    <div class="d-flex justify-content-between" v-if="!isClosed(lot)">
+        <span class="me-2">Time left:</span>
+        {{ timeLeft(lot) }}
+    </div>
+
+    <div class="d-flex justify-content-between">
+        <span class="me-2">Participants:</span>
+        <span>{{ lot.participants }}</span>
+
+    </div>
+    <div class="d-flex justify-content-between" v-if="false">
+        <span class="me-2">Bidded:</span>
+        {{ lot.bidded }}
+    </div>
+    
+    <template v-if="isClosed(lot) && lot.participants > 0">
+        <div class="d-flex justify-content-between">
+            <span class="me-2">Winner:</span>
+            <a :href="`${$web3.blockExplorer}/address/${lot.winner}`" target="_blank" rel="noopener noreferrer" v-if="hasWinner(lot)">
+                {{ $web3.addressShort(lot.winner) }}
+            </a>
+            <span v-else>no winner</span>
+        </div>
+
+        <div class="d-flex justify-content-between" v-if="lot.winBid > 0">
+            <span class="me-2">Win bid:</span>
+            {{ lot.winBid }}
+        </div>
+    </template>
+</template>
+
+<script setup>
+import { timeLeft, isActive, isRevealing, isClosed, hasWinner } from './lot.js'
+
+const { lot } = defineProps({
+    lot: { type: Object, required: true },
+})
+
+</script>
